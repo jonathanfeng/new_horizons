71792df94a7ef4d623e7d3ee9245ff569dcc2878
jonathanfeng <fengjonathany@gmail.com>
2020-03-22 20:34:28 -0700

    csv database lol

--- app/bin/find_bugs.py
+++ app/bin/find_bugs.py
@@ -1,11 +1,8 @@
 from datetime import datetime
 import pytz
+import csv
 
-def main(hemisphere, tz):
-    if hemisphere == 'Northern':
-        bug_list = load_northern()
-    else:
-        bug_list = load_southern()
+def main(tz, bug_list):
     active_bugs = process_bugs(bug_list, datetime.now(pytz.timezone(tz)))
     return active_bugs
 
@@ -19,10 +16,11 @@ def process_bugs(bugs_list, dt):
         good_month = test_seasonality(seasonality, month)
         good_time = test_time(times, hour)
         if good_month and good_time:
-            if bug[3] != 'All day':
-                bug_times = bug[3].split(' - ')
-                bug[3] = bug_times[0] + ':00 - ' + bug_times[1] + ':00'
-            good_bug.append([bug[0], bug[2], int(bug[4].replace(',','')), bug[1], bug[3]])
+            bug_times = bug[3]
+            if bug_times != 'All day':
+                bug_times = bug_times.split(' - ')
+                bug_times = bug_times[0] + ':00 - ' + bug_times[1] + ':00'
+            good_bug.append([bug[0], bug[2], int(bug[4].replace(',','')), bug[1], bug_times])
     return sorted(good_bug, key= lambda x:x[2], reverse=True)
 
 def test_seasonality(seasonality, month):
@@ -76,112 +74,10 @@ def test_time(times, hour):
                     return False
 
 def load_northern():
-    return [
-        ['Agris Butterfly', 'Apr - Sept', 'Flying', '8 - 17', '3,000'],
-        ['Ant', 'All year', 'On rotten fruit and turnips', 'All day', '80'],
-        ['Bagworm', 'All year', 'Shake tree', 'All day', '600'],
-        ['Centipede', 'Sept - June', 'Hit rocks', '17 - 23', '430'],
-        ['Citrus Long-Horned Beetle', 'All year', 'Tree stump', 'All day', '350'],
-        ['Common Butterfly', 'Sept - June', 'Flying', '4 - 19', '160'],
-        ['Cricket', 'Sept - Nov', 'Ground', '17 - 8', '130'],
-        ['Damselfly', 'Nov - Feb', 'Flying', 'All day', '500'],
-        ['Darner Dragonfly', 'Apr - Oct', 'Flying', '8 - 17', '230'],
-        ['Diving Beetle', 'May - Sept', 'River', '8 - 19', '800'],
-        ['Dung Beetle', 'Dec - Feb', 'Snowballs', 'All day', '2,500'],
-        ['Earth-Boring Dung Beetle', 'July - Sept', 'Ground', 'All day', '300'],
-        ['Emperor Butterfly', 'Dec - Mar | June - Sept', 'Flying', '17 - 8', '4,000'],
-        ['Flea', 'Apr - Nov', 'On a flea-infested villager', 'All day', '70'],
-        ['Fly', 'All year', 'On rotten fruit or garbage', 'All day', '60'],
-        ['Grasshopper', 'July - Sept', 'Ground', '8 - 17', '160'],
-        ['Hermit Crab', 'All year', 'Beach', '19 - 8', '1,000'],
-        ['Honeybee', 'Mar - July', 'Near flowers', '8 - 17', '200'],
-        ['Ladybug', 'Mar - June | Oct', 'Flowers', '8 - 17', '200'],
-        ['Long Locust', 'Apr - Nov', 'Ground', '8 - 19', '200'],
-        ['Madagascan Sunset Moth', 'Apr - Sept', 'Flying', '8 - 16', '2,500'],
-        ['Man-Faced Stink Bug', 'Mar - Oct', 'Flowers', '19 - 8', '1,000'],
-        ['Mantis', 'Mar - Nov', 'Flowers', '8 - 17', '430'],
-        ['Migratory Locust', 'Feb - May', 'Ground', '8 - 19', '200'],
-        ['Mole Cricket', 'Nov - May', 'Underground', 'All day', '500'],
-        ['Monarch Butterfly', 'Sept - Nov', 'Flying', '4 - 17', '140'],
-        ['Mosquito', 'June - Sept', 'Flying', '17 - 4', '130'],
-        ['Moth', 'All year', 'Lit windows', '19 - 4', '130'],
-        ['Orchid Mantis', 'Mar - Nov', 'Flowers', '8 - 17', '2,400'],
-        ['Paper Kite Butterfly', 'All year', 'Flying', '8 - 19', '1,000'],
-        ['Peacock Butterfly', 'Mar - June', 'Near black, blue, or purple flowers', '4 - 19', '2,500'],
-        ['Pill Bug', 'Sept - June', 'Hit rocks', '23 - 16', '250'],
-        ['Pondskater', 'May - Sept', 'River', '8 - 19', '130'],
-        ['Queen Alexandra’s Birdwing', 'May - Sept', 'Flying', '8 - 16', '4,000'],
-        ['Rainbow Stag', 'June - Sept', 'Tree', '19 - 8', '6,000'],
-        ['Rajah Brooke’s Birdwing', 'Dec - Feb | Apr - Sept', 'Near black, blue, or purple flowers', '8 - 17', '2,500'],
-        ['Red Dragonfly', 'Sept - Oct', 'Flying', '8 - 19', '180'],
-        ['Rosalia Batsei Beetle', 'May - June', 'Tree stump', 'All day', '3,000'],
-        # ['Scorpion', 'TBD', 'Ground', 'Night', ''],
-        ['Snail', 'All year', 'Rock (rain)', 'All day', '250'],
-        ['Spider', 'All year', 'Shake tree', '19 - 8', '600'],
-        ['Stinkbug', 'Mar - Oct', 'Flowers', 'All day', '120'],
-        ['Tarantula', 'Nov - Apr', 'Ground', '19 - 4', '8,000'],
-        ['Tiger Beetle', 'Feb - Oct', 'Ground', 'All day', '1,500'],
-        ['Tiger Butterfly', 'Mar - Sept', 'Flying', '4 - 19', '240'],
-        ['Violin Beetle', 'May - June | Sept - Nov', 'Tree stump', 'All day', '450'],
-        ['Walker Cicada', 'Aug - Sept', 'Tree', '8 - 16', '400'],
-        ['Walking Leaf', 'July - Sept', 'Ground', 'All day', '600'],
-        ['Wasp', 'All year', 'Shake tree', 'All day', '2,500'],
-        ['Wharf Roach', 'All year', 'Beach (rocks)', 'All day', '200'],
-        ['Yellow Butterfly', 'Mar - June | Sept - Oct', 'Flying', '4 - 19', '160']]
+    return list(csv.reader(open('app/bin/northern_bugs.csv')))
 
 def load_southern():
-    return [
-        ['Agris Butterfly', 'Oct - Mar', 'Flying', '8 - 17', '3,000'],
-        ['Ant', 'All year', 'Rotten fruit or turnips', 'All day', '80'],
-        ['Bagworm', 'All year', 'Shake tree', 'All day', '600'],
-        ['Centipede', 'Mar - Dec', 'Hit rocks', 'All day', '430'],
-        ['Citrus Long-Horned Beetle', 'All year', 'Tree stump', 'All day', '350'],
-        ['Common Butterfly', 'Mar - Dec', 'Flying', '4 - 19', '160'],
-        ['Cricket', 'Mar - May', 'Ground', '17 - 8', '130'],
-        ['Damselfly', 'May - Aug', 'Flying', 'All day', '500'],
-        ['Darner Dragonfly', 'Oct - Apr', 'Flying', '8 - 17', '230'],
-        ['Diving Beetle', 'Nov - Mar', 'River', '8 - 19', '800'],
-        ['Dung Beetle', 'June - Aug', 'Snowballs', 'All day', '2,500'],
-        ['Earth-Boring Dung Beetle', 'Jan - Mar', 'Ground', 'All day', '300'],
-        ['Emperor Butterfly', 'Dec - Mar | June - Sept', 'Flying', '17 - 8', '4,000'],
-        ['Flea', 'Oct - May', 'On a flea-infested Villager', 'All day', '70'],
-        ['Fly', 'All year', 'On rotten fruit or garbage', 'All day', '60'],
-        ['Grasshopper', 'Jan - Mar', 'Ground', '8 - 17', '160'],
-        ['Hermit Crab', 'All year', 'Beach', '19 - 8', '1,000'],
-        ['Honeybee', 'Sept - Jan', 'Near flowers', '8 - 17', '200'],
-        ['Ladybug', 'Apr | Sept - Dec', 'Flowers', '8 - 17', '200'],
-        ['Long Locust', 'Oct - May', 'Ground', '8 - 19', '200'],
-        ['Madagascan Sunset Moth', 'Oct - Mar', 'Flying', '8 - 16', '2,500'],
-        ['Man-Faced Stink Bug', 'Sept - Apr', 'Flowers', '19 - 8', '1,000'],
-        ['Mantis', 'Sept - Apr', 'Flowers', '8 - 17', '430'],
-        ['Migratory Locust', 'Feb - May', 'Ground', '8 - 19', '200'],
-        ['Mole Cricket', 'May - Nov', 'Underground', 'All day', '500'],
-        ['Monarch Butterfly', 'Mar - May', 'Flying', '4 - 17', '140'],
-        ['Mosquito', 'Dec - Mar', 'Flying', '17 - 4', '130'],
-        ['Moth', 'All year', 'Lit windows', '19 - 4', '130'],
-        ['Orchid Mantis', 'Sept - May', 'Flowers', '8 - 17', '2,400'],
-        ['Paper Kite Butterfly', 'All year', 'Flying', '8 - 19', '1,000'],
-        ['Peacock Butterfly', 'Sept - Dec', 'Near black, blue, or purple flowers', '4 - 19', '2,500'],
-        ['Pill Bug', 'Mar - Dec', 'Hit rocks', '23 - 16', '250'],
-        ['Pondskater', 'Nov - Mar', 'River', '8 - 19', '130'],
-        ['Queen Alexandra’s Birdwing', 'Nov - Mar', 'Flying', '8 - 16', '4,000'],
-        ['Rainbow Stag', 'Dec - Mar', 'Tree', '19 - 8', '6,000'],
-        ['Rajah Brooke’s Birdwing', 'June - Aug | Oct - Mar', 'Near black, blue, or purple flowers', '8 - 17', '2,500'],
-        ['Red Dragonfly', 'Mar - Apr', 'Flying', '8 - 19', '180'],
-        ['Rosalia Batsei Beetle', 'Nov - Dec', 'Tree stump', 'All day', '3,000'],
-        # ['Scorpion', 'TBD', 'Ground', 'Night', ''],
-        ['Snail', 'All year', 'Rock (rain)', 'All day', '250'],
-        ['Spider', 'All year', 'Shake tree', '19 - 8', '600'],
-        ['Stinkbug', 'Sept - Apr', 'Flowers', 'All day', '120'],
-        ['Tarantula', 'May - Oct', 'Ground', '19 - 4', '8,000'],
-        ['Tiger Beetle', 'Aug - Apr', 'Ground', 'All day', '1,500'],
-        ['Tiger Butterfly', 'Sept - Mar', 'Flying', '4 - 19', '240'],
-        ['Violin Beetle', 'Mar - May | Nov - Dec', 'Tree stump', 'All day', '450'],
-        ['Walker Cicada', 'Feb - Mar', 'Tree', '8 - 16', '400'],
-        ['Walking Leaf', 'Jan - Mar', 'Ground', 'All day', '600'],
-        ['Wasp', 'All year', 'Shake Tree', 'All day', '2,500'],
-        ['Wharf Roach', 'All year', 'Beach (rocks)', 'All day', '200'],
-        ['Yellow Butterfly', 'Mar - Apr | Sept - Dec', 'Flying', '4 - 19', '160']]
+    return list(csv.reader(open('app/bin/southern_bugs.csv')))
 
 def load_dict():
     return {
--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,12 @@ from app.bin.get_tz import get_timezones
 
 from datetime import datetime
 
+northern_fishes = find_fish.load_northern()
+southern_fishes = find_fish.load_southern()
+northern_bugs = find_bugs.load_northern()
+southern_bugs = find_bugs.load_southern()
+print('Fish and bugs loaded.')
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -17,7 +23,10 @@ def fish():
         hemisphere = request.form.get('hemisphere', None)
         timezone = request.form.get('timezone', None)
         if hemisphere != None and timezone != None:
-            fish_list = find_fish.main(hemisphere, timezone)
+            if hemisphere == 'Northern':
+                fish_list = find_fish.main(timezone, northern_fishes)
+            else:
+                fish_list = find_fish.main(timezone, southern_fishes)
             return render_template('fish-table.html', hemisphere=hemisphere, timezones=get_timezones(), timezone=timezone, fish_list=fish_list)
         return render_template('fish.html', timezones=get_timezones())
     else:
@@ -29,7 +38,10 @@ def bugs():
         hemisphere = request.form.get('hemisphere', None)
         timezone = request.form.get('timezone', None)
         if hemisphere != None and timezone != None:
-            bugs_list = find_bugs.main(hemisphere, timezone)
+            if hemisphere == 'Northern':
+                bugs_list = find_bugs.main(timezone, northern_bugs)
+            else:
+                bugs_list = find_bugs.main(timezone, southern_bugs)
             return render_template('bugs-table.html', hemisphere=hemisphere, timezones=get_timezones(), timezone=timezone, bugs_list=bugs_list)
         return render_template('bugs.html', timezones=get_timezones())
     else:
